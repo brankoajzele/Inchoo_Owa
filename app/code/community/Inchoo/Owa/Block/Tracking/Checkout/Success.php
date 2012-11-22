4115419186c9b710fcaac2b228ecf5360db1712a
Branko Ajzele <ajzele@gmail.com>
2012-11-22 10:26:55 +0100

    Added eCommerce tracking capabilities as described under http://wiki.openwebanalytics.com/index.php?title=Ecommerce_Tracking#Tracking_Delayed_E-commerce_Transactions.

---
+++ app/code/community/Inchoo/Owa/Block/Tracking/Checkout/Success.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Branko Ajzele <ajzele@gmail.com>
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class Inchoo_Owa_Block_Tracking_Checkout_Success extends Mage_Core_Block_Template
+{
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('inchoo/owa/tracking/checkout/success.phtml');
+    }
+    
+    protected function _toHtml()
+    {
+        if (!$this->helper('inchoo_owa')->isModuleOutputEnabled()) {
+            return '';
+        }
+        
+        return parent::_toHtml();
+    }    
+}
